fb2435560e6e4b9bf51740fba216f0ff86f5805c
shrajaee2 <shrajaee2@gmail.com>
2019-01-08 09:18:01 +0330

    first commit

---
+++ DisplayDetails.groovy
@@ -0,0 +1,6 @@
+ 
+Email email = new Email()
+email.name= "sh"
+email.address  = "1@1.com"
+email.sendDate= new Date()
+println email.displayContents()
---
+++ Email.groovy
@@ -0,0 +1,9 @@
+class Email{
+    String name
+    String address
+    Date sendDate
+    String note
+    String displayContents(){
+         " Name=$name \n address=$address \n sendDate=$sendDate \n note= $note"
+    }
+}
